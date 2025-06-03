f0b3b9c78c752dc6f6d48e73f9ff7eb48bad7a41
kamusia <adamhorolov36027@gmail.com>
2025-06-04 00:45:19 +0300

    step 3 fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'crispy_forms',
-    'crispy_bootstrap5',
     'task_manager',
     'task_manager.users',
 ]
