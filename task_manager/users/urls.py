1be38162a5ffe8850e0f5642f7e041ad80297480
kamusia <adamhorolov36027@gmail.com>
2025-06-03 13:08:18 +0300

    fix step 3

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('', views.UserListView.as_view(), name='list'),
     path('create/', views.UserCreateView.as_view(), name='create'),
