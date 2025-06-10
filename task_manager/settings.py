211f5a00b1f68813a6eb8f5981c050cc96f3e24f
kamusia <adamhorolov36027@gmail.com>
2025-06-11 00:40:10 +0300

    step 3 fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -129,3 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'users_list'
+LOGOUT_REDIRECT_URL = 'home'
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -5,6 +5,19 @@ from django.urls import reverse_lazy
 from .models import User
 from .forms import UserCreateForm, UserLoginForm
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
+from django.shortcuts import redirect
+
+
+class UserOwnerMixin(LoginRequiredMixin, UserPassesTestMixin):
+    error_message = 'У вас нет прав для изменения другого пользователя.'
+
+    def test_func(self):
+        return self.request.user == self.get_object()
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.error_message)
+        return redirect('users:list')
 
 
 class HomePageView(TemplateView):
@@ -21,31 +34,29 @@ class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserCreateForm
     template_name = 'users/create.html'
     success_url = reverse_lazy('login')
-    success_message = 'Пользователь успешно создан!'
+    success_message = 'Пользователь успешно создан'
 
 
-class UserLoginView(LoginView):
+class UserLoginView(SuccessMessageMixin, LoginView):
     form_class = UserLoginForm
     template_name = 'users/login.html'
-    next_page = reverse_lazy('home')
+    success_message = 'Вы залогинены'
 
 
-class UserLogoutView(LogoutView):
-    next_page = reverse_lazy('home')
+class UserLogoutView(SuccessMessageMixin, LogoutView):
+    success_message = 'Вы разлогинены'
 
 
-class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, SuccessMessageMixin, UpdateView):
+class UserUpdateView(UserOwnerMixin, SuccessMessageMixin, UpdateView):
     model = User
     form_class = UserCreateForm
     template_name = 'users/update.html'
     success_url = reverse_lazy('users:list')
-    success_message = 'Пользователь успешно изменен!'
+    success_message = 'Пользователь успешно изменен'
 
 
-class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+class UserDeleteView(UserOwnerMixin, SuccessMessageMixin, DeleteView):
     model = User
     template_name = 'users/delete.html'
     success_url = reverse_lazy('users:list')
-
-    def test_func(self):
-        return self.get_object() == self.request.user
+    success_message = 'Пользователь успешно удален'
