c9f3272146bf6016dd6799d3ccf3a2b83e356ed6
kamusia <adamhorolov36027@gmail.com>
2025-06-12 16:11:19 +0300

    fix step 3

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -9,11 +9,6 @@ class UserCreateForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['password1'].validators = [MinLengthValidator(3)]
-
-        self.fields['password1'].help_text = 'Минимум 3 символа'
-        self.fields['password2'].help_text = 'Повторите пароль для подтверждения'
-
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
 
@@ -21,17 +16,12 @@ class UserCreateForm(UserCreationForm):
         model = User
         fields = ('username', 'first_name',
                   'last_name', 'password1', 'password2')
-        help_texts = {
-            'username': 'Минимум 3 символа. Только буквы, цифры и @/./+/-/_',
-        }
 
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(
-        label='Имя пользователя',
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     password = forms.CharField(
-        label='Пароль',
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
