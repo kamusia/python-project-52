6a75ced7853edd73635a2399c2414dbb98858c31
kamusia <adamhorolov36027@gmail.com>
2025-06-26 00:08:00 +0300

    fix step 5

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('create/', views.TaskCreateView.as_view(), name='create'),
     path('<int:pk>/update/', views.TaskUpdateView.as_view(), name='update'),
     path('<int:pk>/delete/', views.TaskDeleteView.as_view(), name='delete'),
-    path('<int:pk>/', views.TaskDetailView.as_view(), name='task_detail')
+    path('<int:pk>/', views.TaskDetailView.as_view(), name='detail')
 ]
